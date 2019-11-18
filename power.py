ea86f95d047bc37d8542187992106777b1454c4f
dipesh1011 <56926563+dipesh1011@users.noreply.github.com>
2019-11-18 06:49:27 -0800

    Add files via upload
    Calculating any power

---
+++ power.py
@@ -0,0 +1,7 @@
+def power(a,b):
+    return a ** b
+
+n1 = float(input("Enter value for base:"))
+n2 = float(input("Enter value for power:"))
+pow = power(n1,n2)
+print("The result value is:","%.2f" %pow)
